b05a5f0d22e88b96f08b40b606003aad63d60b98
João Guilherme <160131661+joaogcsilva@users.noreply.github.com>
2024-08-09 10:45:12 -0300

    Add files via upload

---
+++ cliente/index.php
@@ -0,0 +1,12 @@
+<?php
+   include "../admin/acesso_com.php"
+?>
+<h2>
+<strong><?php echo $_GET['cliente']; ?></strong>, Bem vindo à área de cliente! 
+</h2>
+<a href="../admin/logout.php">
+        <span class="glyphicon glyphicon-log-out">Sair</span>
+    </a>
+
+
+
